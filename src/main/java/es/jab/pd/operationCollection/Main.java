f0f7a31886614981f1915d2f434b3fc791ed51fe
Javier Combarros <Javier@138.4.192.251>
2014-10-13 23:19:55 +0200

    Main modified adding Multiplication operations The application is still running properly

--- src/main/java/es/jab/pd/operationCollection/Main.java
+++ src/main/java/es/jab/pd/operationCollection/Main.java
@@ -7,12 +7,15 @@ public class Main {
 		operationHandler.add(new Addition(3, 4));
 
         System.out.println("Total1:" + operationHandler.total());
-
+        
+        operationHandler.reset();
         operationHandler.add(new Subtraction(1, 1));
         operationHandler.add(new Addition(3, 5));
+        operationHandler.add(new Multiplication(1, 2));
         operationHandler.add(new Subtraction(4, 1));
         operationHandler.add(new Addition(3, 6));
         operationHandler.add(new Subtraction(3, 2));
+        operationHandler.add(new Multiplication(3, 6));
 
         System.out.println("Total2:" + operationHandler.total());
 	}
