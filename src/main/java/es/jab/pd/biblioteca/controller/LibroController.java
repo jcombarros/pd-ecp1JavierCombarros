566aa4982b04c3817205bfe05f539b39a4c9729d
Javier Combarros <Javier@138.4.192.251>
2014-10-14 23:21:55 +0200

    LibroController Implemented

--- src/main/java/es/jab/pd/biblioteca/controller/LibroController.java
+++ src/main/java/es/jab/pd/biblioteca/controller/LibroController.java
@@ -1,5 +1,9 @@
 package es.jab.pd.biblioteca.controller;
 
+import java.util.List;
+
+import upm.jbb.IO;
+import es.jab.pd.biblioteca.model.Libro;
 import es.jab.pd.biblioteca.service.GestorLibro;
 
 public class LibroController {
@@ -11,19 +15,30 @@ public class LibroController {
 	}
 	
 	public void darAltaLibro(){
-		
+		Libro libro = (Libro) IO.in.read(Libro.class, "Crear un nuevo Libro: ");
+		gestorLibro.darAltaLibro(libro);
+		IO.out.println("Libro añadido a la biblioteca.");
 	}
 	
 	public void darBajaLibro(){
+		Libro libro = (Libro) IO.in.select((Libro[])gestorLibro.getLibros().toArray(), "Seleccionar un libro para eliminar: ");
+		gestorLibro.darBajaLibro(libro);
+		IO.out.println("Libro eliminado de la biblioteca.");
 		
 	}
 	
 	public void mostrarLibros(){
-		
+		List<Libro> libros = gestorLibro.mostrarLibros();
+		for(Libro libro : libros){
+			IO.out.println("Libro: " + libro);
+		}
 	}
 	
 	public void mostrarLibro(){
-		
+		//suponemos que recuperamos el isbn para usar el servicio de mostrarLibro() por isbn
+		String isbn = ((Libro) IO.in.select((Libro[])gestorLibro.getLibros().toArray(), "Seleccionar un libro para eliminar: ")).getIsbn();
+		Libro libro = gestorLibro.mostrarLibro(isbn);
+		IO.out.println("Libro seleccionado: " + libro.toString());
 	}
 
 }
